88aff7f747c4f7b7edfef86a78b2f4c0718e8470
Alamin-git <alamin2.mas@gmail.com>
2022-06-20 22:10:25 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
+
+// mongodb
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://doctors_admin:<password>@cluster0.or6bv.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 
 app.get("/", (req, res) => {
    res.send("Doctors-Portal");
