d1e42e78b55015df8888743e316163c2f180c861
Alamin <alamin2.mas@gmail.com>
2023-03-11 21:35:11 +0600

    booking appointment now ready

--- index.js
+++ index.js
@@ -1,44 +1,59 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
-require('dotenv').config();
-
+require("dotenv").config();
 
 const app = express();
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.or6bv.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try{
-        const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
+   try {
+      const appointmentOptionCollection = client
+         .db("doctorsPortal")
+         .collection("appointmentOptions");
+      const bookingsCollection = client
+         .db("doctorsPortal")
+         .collection("bookings");
 
-        app.get('/appointmentOptions', async(req, res ) =>{
-            const query = {};
-            const options = await appointmentOptionCollection.find(query).toArray();
-            res.send(options);
-        })
+      app.get("/appointmentOptions", async (req, res) => {
+         const query = {};
+         const options = await appointmentOptionCollection
+            .find(query)
+            .toArray();
+         res.send(options);
+      });
 
-    }
-    finally{}
-};
+      app.post('/bookings', async(req, res) =>{
+        const booking =req.body
+        console.log(booking);
+        const result = await bookingsCollection.insertOne(booking);
+        res.send(result);
+      })
+      /***
+       * API Naming Convention
+       *app.get('/bookings')
+       * app.get('/bookings:id')
+       * app.post('/bookings')
+       * app.patch('/bookings:id')
+       * app.delete('/bookings:id')
+       */
+   } finally {
+   }
+}
 run().catch(console.log);
 
-
-
-
-
-app.get('/', async(req, res) => {
-    res.send('doctors portal server is running')
+app.get("/", async (req, res) => {
+   res.send("doctors portal server is running");
 });
 app.listen(port, () => console.log(`Doctors portal running on ${port}`));
