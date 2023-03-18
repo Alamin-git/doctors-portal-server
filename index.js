e2bd5279a23d1e2df58fc8994b027b27201a54b8
Alamin <alamin2.mas@gmail.com>
2023-03-18 22:12:01 +0600

    jwt token create

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
+
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 
 const app = express();
@@ -92,6 +94,20 @@ async function run() {
           const user =req.body;
           const result = await usersCollection.insertOne(user);
           res.send(result);
+      });
+
+
+      app.get('/jwt', async(req,res)=>{
+         const email =req.query.email;
+         const query = {email:email};
+         const user = await usersCollection.findOne(query);
+         if(user){
+            const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
+            return res.send({accessToken : 'token'})
+         }
+         console.log(user);
+         res.status(403).send({accessToken: ''})
+
       })
    } finally {
    }
